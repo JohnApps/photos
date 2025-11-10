8b56ca3df0365236e35ca02dc9137ec657206b61
JohnApps <johndapps@gmail.com>
2025-11-10 17:14:58 +0100

    first commit

---
+++ set_env.bat
@@ -0,0 +1,13 @@
+echo off
+rem set_env.bat
+rem https://duckdb.org/docs/extensions/postgres
+rem enter envs for pg
+rem
+title PG_PHOTOS
+rem chcp 1252
+chcp 437
+set PGDATABASE=photos
+set pghost = localhost
+set PGUSER=appsj
+set PGPASSWORD=digital
+prompt photos$G
